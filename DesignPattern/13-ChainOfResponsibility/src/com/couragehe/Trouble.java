ce9308b8a8d5d3eec1d86b1cc2eadfc114724831
CourageHe <courage.he@gmail.com>
2022-03-27 15:40:43 +0800

    ChainOfResponsibility模式

---
+++ DesignPattern/13-ChainOfResponsibility/src/com/couragehe/Trouble.java
@@ -0,0 +1,18 @@
+package com.couragehe;
+/*
+ * 表示发生问题的类
+ */
+public class Trouble {
+	private int number;
+	public Trouble(int number) { 
+		this.number = number;
+	}
+	public int getNumber() {
+		return number;
+	}
+	@Override
+	public String toString() {
+		return "Trouble [number=" + number + "]";
+	}
+	
+}
